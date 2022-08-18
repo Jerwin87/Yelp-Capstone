15837313f59311f247ed3365dbf0218dca07ed0d
Jeremy Winandy <jeremy.winandy@gmail.com>
2022-08-18 12:01:03 +0200

    processign file

--- modeling/processing.py
+++ modeling/processing.py
@@ -1,10 +1,32 @@
-# Dataframes
 import pandas as pd
-import numpy as np
-
-# Language detection
+import json
 import fasttext as ft
 from pycountry import pycountry
+
+
+# define function to reed json
+def get_df(fn, limit=None):
+    json_lines = []
+    line_nr = 1
+    with open(fn) as f:
+        for line in f:
+            if limit and line_nr == limit:
+                break
+            json_line = json.loads(line)
+            json_lines.append(json_line)
+            line_nr += 1
+    df = pd.DataFrame(json_lines)
+    return df
+
+# define a list with all symbols to be removed
+punctuation = ['?', '.', ':', ':', '!', '"', '(', ')', '-', '$', ',', '+', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+# function to remove these symbols
+def remove_punctuation(text):    
+    cleaned_text = "".join(u for u in text if u not in punctuation)
+    return cleaned_text
+
+# Language detection
 
 def language_processing(df_lang, verbose=False, cleaned=True, accuracy=0.95):
 
