8db0b5c976944a830f7103812d6c729e3053dee7
Maximilian Kinzler <llangardaix@mailbox.org>
2022-08-19 09:39:34 +0200

    change key list

--- notebooks/mk_categories_word2vec.py
+++ notebooks/mk_categories_word2vec.py
@@ -8,7 +8,7 @@ sys.path.append('..')
 from modeling.processing import get_df
 
 def train():
-    df = get_df('/Users/mkinzler/neuefische/Yelp-Capstone/data/yelp_academic_dataset_business.json', limit=None)
+    df = get_df('../data/yelp_dataset/yelp_academic_dataset_business.json', limit=None)
     cats = df.categories.dropna().values # What form do the NAs have?
     
     cats_separated = [ [token.strip() for token in item.lower().split(',')] for item in cats] # remove commas in a different way?
@@ -17,53 +17,54 @@ def train():
     model = Word2Vec(sentences=cats_separated, vector_size=100, window=3, min_count=1, workers=4)
     model.save("cat_word2vec.model")
 
-# train()
+train()
 
 model = Word2Vec.load("cat_word2vec.model")
 # print(model.wv.key_to_index)
 
-### # adapted from https://towardsdatascience.com/google-news-and-leo-tolstoy-visualizing-word2vec-word-embeddings-with-t-sne-11558d8bd4d
-### keys = ['restaurants', 'breweries', 'shopping', 'doctors']
-### embedding_clusters = []
-### word_clusters = []
-### 
-### for word in keys:
-###     embeddings = []
-###     similiar_words = []
-###     for sim_word, _ in model.wv.most_similar(word, topn=20):
-###         similiar_words.append(sim_word)
-###         embeddings.append(model.wv[sim_word])
-###     word_clusters.append(similiar_words)
-###     embedding_clusters.append(embeddings)
-###         
-### # print(similiar_words)
-### from sklearn.manifold import TSNE
-### import numpy as np
-### 
-### embedding_clusters = np.array(embedding_clusters)
-### n, m, k = embedding_clusters.shape
-### print(n, m, k)
-### tsne_model_2d = TSNE(perplexity=15, n_components=2, init='pca', n_iter=3500, random_state=42)
-### embeddings_2d = np.array(tsne_model_2d.fit_transform(embedding_clusters.reshape(n*m, k))).reshape(n, m, 2)
-### 
-### import matplotlib.pyplot as plt
-### import matplotlib.cm as cm
-### from mpl_toolkits.mplot3d import Axes3D
-### 
-### 
-### a = 0.7
-### plt.figure(figsize=(16, 9))
-### colors = cm.rainbow(np.linspace(0, 1, len(keys)))
-### for label, embeddings, words, color in zip(keys, embeddings_2d, word_clusters, colors):
-###     x = embeddings[:, 0]
-###     y = embeddings[:, 1]
-###     plt.scatter(x, y, c=color, alpha=a, label=label)
-###     for i, word in enumerate(words):
-###         plt.annotate(word, alpha=0.5, xy=(x[i], y[i]), xytext=(5, 2),
-###                      textcoords='offset points', ha='right', va='bottom', size=8)
-### plt.savefig('cat_word2vec.png', format='png', dpi=150, bbox_inches='tight')
-### plt.show()
-### 
+# adapted from https://towardsdatascience.com/google-news-and-leo-tolstoy-visualizing-word2vec-word-embeddings-with-t-sne-11558d8bd4d
+# keys = ['restaurants', 'breweries', 'shopping', 'doctors']
+keys = ['restaurants', 'bars', 'shopping', 'doctors']
+embedding_clusters = []
+word_clusters = []
+
+for word in keys:
+    embeddings = []
+    similiar_words = []
+    for sim_word, _ in model.wv.most_similar(word, topn=100):
+        similiar_words.append(sim_word)
+        embeddings.append(model.wv[sim_word])
+    word_clusters.append(similiar_words)
+    embedding_clusters.append(embeddings)
+        
+# print(similiar_words)
+from sklearn.manifold import TSNE
+import numpy as np
+
+embedding_clusters = np.array(embedding_clusters)
+n, m, k = embedding_clusters.shape
+print(n, m, k)
+tsne_model_2d = TSNE(perplexity=15, n_components=2, init='pca', n_iter=3500, random_state=42)
+embeddings_2d = np.array(tsne_model_2d.fit_transform(embedding_clusters.reshape(n*m, k))).reshape(n, m, 2)
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
+
+
+a = 0.7
+plt.figure(figsize=(16, 9))
+colors = cm.rainbow(np.linspace(0, 1, len(keys)))
+for label, embeddings, words, color in zip(keys, embeddings_2d, word_clusters, colors):
+    x = embeddings[:, 0]
+    y = embeddings[:, 1]
+    plt.scatter(x, y, c=color, alpha=a, label=label)
+    for i, word in enumerate(words):
+        plt.annotate(word, alpha=0.5, xy=(x[i], y[i]), xytext=(5, 2),
+                     textcoords='offset points', ha='right', va='bottom', size=8)
+plt.savefig('cat_word2vec.png', format='png', dpi=150, bbox_inches='tight')
+plt.show()
+
 ### # tsne_model_3d = TSNE(perplexity=30, n_components=3, init='pca', n_iter=3500, random_state=42)
 ### # # embeddings_3d = tsne_model_3d.fit_transform(embedding_clusters)
 ### # embeddings_3d = tsne_model_3d.fit_transform(embedding_clusters.reshape(n*m, k)).reshape(n, m, 3)
@@ -86,32 +87,32 @@ model = Word2Vec.load("cat_word2vec.model")
 ### # 
 ### # plt.show()
 
-# clustering K-means
-# inspirated by https://dylancastillo.co/nlp-snippets-cluster-documents-using-word2vec/
-import numpy as np
-
-df = get_df('/Users/mkinzler/neuefische/Yelp-Capstone/data/yelp_academic_dataset_business.json', limit=None)
-cats = df.categories.dropna().values # What form do the NAs have?
-    
-cats_separated = [ [token.strip() for token in item.lower().split(',')] for item in cats] # remove commas in a different way?
-
-doc_vectors_mean = []
-
-for doc in cats_separated[0:100]:
-    doc_vectors = []
-    for token in doc:
-        doc_vectors.append(model.wv[token])
-    doc_vectors_mean.append(np.mean(doc_vectors, axis=0)) # we also need some labels…
-
-from sklearn.cluster import MiniBatchKMeans
-from sklearn.metrics import silhouette_samples, silhouette_score
-
-K = 5
-km = MiniBatchKMeans(n_clusters=K, batch_size=50).fit(doc_vectors_mean)
-silhouette_samples_values = silhouette_samples(doc_vectors_mean, km.labels_)
-for i in range(K):
-    cluster_sv = silhouette_samples_values[km.labels_ == i]
-    print(i, cluster_sv.shape[0], cluster_sv.mean(), cluster_sv.min(), cluster_sv.max())
+# # clustering K-means
+# # inspirated by https://dylancastillo.co/nlp-snippets-cluster-documents-using-word2vec/
+# import numpy as np
+# 
+# df = get_df('/Users/mkinzler/neuefische/Yelp-Capstone/data/yelp_academic_dataset_business.json', limit=None)
+# cats = df.categories.dropna().values # What form do the NAs have?
+#     
+# cats_separated = [ [token.strip() for token in item.lower().split(',')] for item in cats] # remove commas in a different way?
+# 
+# doc_vectors_mean = []
+# 
+# for doc in cats_separated[0:100]:
+#     doc_vectors = []
+#     for token in doc:
+#         doc_vectors.append(model.wv[token])
+#     doc_vectors_mean.append(np.mean(doc_vectors, axis=0)) # we also need some labels…
+# 
+# from sklearn.cluster import MiniBatchKMeans
+# from sklearn.metrics import silhouette_samples, silhouette_score
+# 
+# K = 5
+# km = MiniBatchKMeans(n_clusters=K, batch_size=50).fit(doc_vectors_mean)
+# silhouette_samples_values = silhouette_samples(doc_vectors_mean, km.labels_)
+# for i in range(K):
+#     cluster_sv = silhouette_samples_values[km.labels_ == i]
+#     print(i, cluster_sv.shape[0], cluster_sv.mean(), cluster_sv.min(), cluster_sv.max())
 
 
 
